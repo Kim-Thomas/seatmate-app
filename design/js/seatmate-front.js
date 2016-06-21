401500186795cf8ae3dc2e4bc0817ec8ca8bcc11
Thomas KIM <thomas.kim@telecomnancy.net>
2016-06-21 21:40:50 +0200

    Improved input recognition to detect flight numbers and seat numbers.

--- design/js/seatmate-front.js
+++ design/js/seatmate-front.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $('input').on('keyup', function() {
         var value = $(this).val();
-        if(value.length > 0) {
+        var int_matches = value.match(/(\d)/);
+        var chars_matches = value.match(/[a-zA-Z]/);
+        if(int_matches != null && chars_matches != null) {
             $(this).parent().find('i').addClass('correct');
         } else {
             $(this).parent().find('i').removeClass('correct');
